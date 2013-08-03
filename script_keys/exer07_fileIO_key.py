271a74b675a698afd75648c8bd051ec7e581d518
hongtao510 <hongtao510@gmail.com>
2013-08-03 00:35:07 -0400

    update

--- script_keys/exer07_fileIO_key.py
+++ script_keys/exer07_fileIO_key.py
@@ -10,13 +10,14 @@
 # 2.1 Read IntegerFile.txt, which has many numbers on one line.
 # 2.2 Calculate the sum, and the mean. Hint, the int() function
 # turns a string into a number.
-# num_all=[]
-# file = open("IntegerFile.txt")
-# for line in file:
-# 	for word in line.split():
-# 		num_all.append(int(word))
-# file.close()
-# print sum(num_all)
+num_all=[]
+file = open("IntegerFile.txt")
+for line in file:
+	for word in line.split():
+		num_all.append(int(word))
+file.close()
+print sum(num_all)
+print sum(num_all).mean()
 
 # 3 File copier
 # 3.1 Copy http://www.ubertool.org/index.html to a file on your local disk.
