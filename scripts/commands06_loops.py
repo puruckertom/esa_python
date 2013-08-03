ac0aeb795cf234f4eb8d603fad927ae057611c9f
Tom Purucker <purucker.tom@gmail.com>
2013-08-03 17:23:17 -0400

    post-course cleanup

--- scripts/commands06_loops.py
+++ scripts/commands06_loops.py
@@ -18,6 +18,15 @@ print(bool("alpha"<"beta"))
 watersheds = ["Suwanne", "Oconee", "Tennessee", "Flint"]
 for index, value in enumerate(watersheds):
   print index, value
+
+a = 9
+b = 0
+c = []
+while b < 100:
+  b = b + 5
+  a = a + 1
+  c. append(b)
+  print b
   
 # break
 for number in range(1,8):
@@ -28,6 +37,9 @@ for number in range(1,8):
 
 # continue
 for number in range(1,8):
+    if number == 5:
+      continue
+    print number
  if number < 5:
    print number
    continue
